12b180501109f3ba94f31855d0c4afbc9f4df49e
Joseph Safari <sajo2460hotmail.com>
2020-02-21 17:00:04 +0000

    Configuration to send geolocation in index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', (socket) => {
+iio.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
@@ -20,6 +20,10 @@ io.on('connection', (socket) => {
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 
     socket.on('disconnect', () => {
